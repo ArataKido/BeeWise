dbd14948c29583936bf256824d74a89fa0d10d5d
Arata <shahzod20030905@gmail.com>
2023-10-15 12:07:45 +0200

    added migrations

--- app/services/migrations/env.py
+++ app/services/migrations/env.py
@@ -34,7 +34,7 @@ def run_migrations_offline() -> None:
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
     )
-
+    
     with context.begin_transaction():
         context.run_migrations()
 
