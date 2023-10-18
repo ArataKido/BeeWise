17f9b69b0b5358f63b6a85ad552838aa13a89914
Arata <shahzod20030905@gmail.com>
2023-10-18 23:31:56 +0300

    added return type

--- app/routers/quiz.py
+++ app/routers/quiz.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.controllers.quiz import make_request, add_questions
+from app.services.schema.quiz import QuizQuestionSchema
 
 router = APIRouter()
 
@@ -8,6 +9,6 @@ router = APIRouter()
 @router.post("/bring_questions")
 async def parce_questions(
     questions_num: int,
-):
+) -> QuizQuestionSchema | None :
     if questions := make_request(questions_num=questions_num):
         return await add_questions(questions)
